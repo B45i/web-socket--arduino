7777092dda50a3eb2bf8d997443a21e2ed6761cd
b45i <B45i>
2019-08-04 23:28:22 +0530

    file path change

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+const path = require('path')
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-	res.sendfile('index.html');
-});
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath);
 
 
 let clients = 0;
